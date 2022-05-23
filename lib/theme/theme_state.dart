1078e6d699fce08576a4398f7bae7854de156038
Hery <thitphavanh.psv@gmail.com>
2022-05-23 20:47:35 +0700

    first commit

---
+++ lib/theme/theme_state.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+part of 'theme_bloc.dart';
+
+enum Apptheme {
+  light,
+  dark,
+}
+
+class ThemeState extends Equatable {
+  final Apptheme appTheme;
+  ThemeState({
+    this.appTheme = Apptheme.light,
+  });
+
+  factory ThemeState.initial() {
+    return ThemeState();
+  }
+
+  @override
+  List<Object?> get props => [appTheme];
+
+  @override
+  String toString() => 'ThemeState(appTheme: $appTheme)';
+
+  ThemeState copyWith({
+    Apptheme? appTheme,
+  }) {
+    return ThemeState(
+      appTheme: appTheme ?? this.appTheme,
+    );
+  }
+}
